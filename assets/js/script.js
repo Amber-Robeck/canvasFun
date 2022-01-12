bb88bf1b1a1f6828b0c886bef2534371c2dbb8ff
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-01-11 20:11:37 -0600

    added event listener to click and mousemove to call draw function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("mainCanvas");
 var ctx = canvas.getContext('2d');
-
+//draw does not function properly without this line
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 //from mdn
 // function draw() {
 //     var ctx = document.getElementById('canvas').getContext('2d');
@@ -15,3 +17,43 @@ var ctx = canvas.getContext('2d');
 //     ctx.fillText('Hello world', 10, 50);
 // }
 // draw();
+
+// shows 2dobj
+// console.log(ctx);
+
+
+var mouseClick = {
+    x: undefined,
+    y: undefined,
+}
+
+
+//onclick draw
+// canvas.addEventListener("click", function (event) {
+//     console.log("working")
+
+//     mouseClick.x = event.x;
+//     mouseClick.y = event.y;
+//     draw();
+// });
+
+//onmousemove draw
+canvas.addEventListener("mousemove", function (event) {
+    mouseClick.x = event.x;
+    mouseClick.y = event.y;
+    draw();
+}
+)
+function draw() {
+    ctx.fillStyle = "gray";
+    ctx.strokeStyle = "darkGray";
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.arc(mouseClick.x, mouseClick.y, 50, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.fill();
+}
+
+
+
+
