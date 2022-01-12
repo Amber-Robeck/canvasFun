8f6406e7001cfbf77e08efca18454813977665c8
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-01-11 19:19:25 -0600

    added draw function from mdn and tested fillText is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-//
+var canvas = document.getElementById("mainCanvas");
+var ctx = canvas.getContext('2d');
+
+//from mdn
+// function draw() {
+//     var ctx = document.getElementById('canvas').getContext('2d');
+//     ctx.font = '48px serif';
+//     ctx.fillText('Hello world', 10, 50);
+
+
+//displays hello world within draw function
+
+// function draw() {
+//     ctx.font = '25px serif';
+//     ctx.fillText('Hello world', 10, 50);
+// }
+// draw();
